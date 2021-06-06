b992c63cd3486b075b1c5a5b250e13ef516e50bb
Ricardo Fontenla <ric_parada@hotmail.com>
2021-06-06 04:24:21 -0300

    WIP admin manages payment methods: activation of active storage and test refactoring

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,3 +1,5 @@
 class PaymentMethod < ApplicationRecord
+  has_one_attached :category_icon
+  
   enum category: { boleto: 1, card: 2, pix: 3 }
 end
--- spec/system/admin_view_payment_methods_spec.rb
+++ spec/system/admin_view_payment_methods_spec.rb
@@ -2,40 +2,55 @@ require 'rails_helper'
 
 describe "Admin view payment methods" do
   it 'sucessfully' do
-    PaymentMethod.create!(name: 'Boleto do Banco Laranja', 
-                          billing_fee: 2.5, 
-                          max_fee: 100.0,
-                          active: true,
-                          category: :boleto)
-    PaymentMethod.create!(name: 'PISA', 
-                          billing_fee: 5, 
-                          max_fee: 250,
-                          active: false,
-                          category: 2)
+    boleto = PaymentMethod.create!(name: 'Boleto do Banco Laranja', 
+                                   billing_fee: 2.5, 
+                                   max_fee: 100.0,
+                                   active: true,
+                                   category: :boleto)
+    boleto.category_icon
+            .attach(io: File.open('spec/fixtures/boleto.png'), 
+                    filename: 'boleto.png')
+
+    card = PaymentMethod.create!(name: 'PISA', 
+                                 billing_fee: 5, 
+                                 max_fee: 250,
+                                 active: false,
+                                 category: 2)
+    card.category_icon
+          .attach(io: File.open('spec/fixtures/card.png'), 
+                  filename: 'card.png')
 
     admin_login
     visit root_path
-    save_page
     click_on 'Métodos de Pagamento'
-
+    
     expect(page).to have_content('Gerenciar Métodos de Pagamentos')
     expect(page).to have_content('Boleto do Banco Laranja')
     expect(page).to have_content('Ativo')
+    expect(page).to have_css("img[src*='boleto.png']")
     expect(page).to have_content('PISA')
     expect(page).to have_content('Inativo')
+    expect(page).to have_css("img[src*='card.png']")
   end
 
   it 'and view details' do
-    PaymentMethod.create!(name: 'Boleto do Banco Laranja', 
-                          billing_fee: 2.5, 
-                          max_fee: 100.0,
-                          active: true,
-                          category: :boleto)
-    method = PaymentMethod.create!(name: 'PISA', 
-                                   billing_fee: 5, 
-                                   max_fee: 250,
-                                   active: false,
-                                   category: :card)
+    boleto = PaymentMethod.create!(name: 'Boleto do Banco Laranja', 
+                                   billing_fee: 2.5, 
+                                   max_fee: 100.0,
+                                   active: true,
+                                   category: :boleto)
+    boleto.category_icon
+            .attach(io: File.open('spec/fixtures/boleto.png'), 
+                    filename: 'boleto.png')
+
+    card = PaymentMethod.create!(name: 'PISA', 
+                                 billing_fee: 5, 
+                                 max_fee: 250,
+                                 active: false,
+                                 category: 2)
+    card.category_icon
+          .attach(io: File.open('spec/fixtures/card.png'), 
+                  filename: 'card.png')
 
     admin_login
     visit admin_payment_methods_path
@@ -47,6 +62,7 @@ describe "Admin view payment methods" do
     expect(page).to have_content('R$ 250,00')
     expect(page).to have_content('Inativo')
     expect(page).to have_content('Cartão')
+    expect(page).to have_css("img[src*='card.png']")
   end
 
   it 'and no payment methods in the system' do
